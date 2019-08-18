9d715ca60204e5e0c73f21941feac4c22100aa3e
Martin Sundeqvist <martin.sundequist@gmail.com>
2019-08-18 21:37:26 +0200

    Seems to work now

--- estring.c
+++ estring.c
@@ -10,14 +10,18 @@ SplitStringStruct *split_string(char *string, char delimiter) {
     int buffer_len;
     char **elements = NULL;
     int n_elements;
+    int finished_parsing = 0;
 
     buffer_len = 1;
     buffer = (char *) malloc(sizeof(char));
     buffer[buffer_len-1] = '\0';
     n_elements = 0;
-    for (i = 0; c != EOF; i++) {
+    i = 0;
+    while (!finished_parsing) {
         c = string[i];
-	if (c == delimiter) {
+	if (c == delimiter || c == EOF) {
+	    printf("hellu %c\n", c);
+	    fflush(stdout);
 	    n_elements++;
 	    elements = (char **) realloc(elements, sizeof(char *) * n_elements);
 	    elements[n_elements-1] = (char *) malloc(sizeof(char) * buffer_len);
@@ -27,12 +31,17 @@ SplitStringStruct *split_string(char *string, char delimiter) {
 	    buffer_len = 1;
             buffer = (char *) malloc(sizeof(char));
 	    buffer[buffer_len-1] = '\0';
+	    finished_parsing = c == EOF;
 	} else {
 	    buffer_len++;
 	    buffer = (char *) realloc(buffer, sizeof(char) * buffer_len);
 	    buffer[buffer_len-1] = '\0';
 	    buffer[buffer_len-2] = c;
 	}
+	i++;
     }
+
+    split_struct->string_list = elements;
+    split_struct->n_elements = n_elements;
     return split_struct;
 }
--- main.c
+++ main.c
@@ -1,5 +1,10 @@
+#include <stdio.h>
 #include "estring.h"
 
 int main(int argc, char **argv) {
-    split_string("123,456,789", ',');
+    SplitStringStruct *split_string_struct =  split_string("123,456,789", ',');
+    printf("string list 1: %s\n", split_string_struct->string_list[0]);
+    printf("string list 2: %s\n", split_string_struct->string_list[1]);
+    printf("string list 3: %s\n", split_string_struct->string_list[2]);
+    printf("number of elements %d\n", split_string_struct->n_elements);
 }
